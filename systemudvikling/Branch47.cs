8d04f93fc652252b04aa917588d80eb3b9f817c5
Charlotte Østergaard Sabransky <chs023@edu.zealand.dk>
2025-11-14 12:22:11 +0100

    New class

---
+++ systemudvikling/Branch47.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace systemudvikling
+{
+    public class Branch47
+    {
+        Console.Writeline("Hej");
+    }
+}
