518ed640eb4cb38cbe462c7fcd7eaf880625cb38
lbstok <stoklas0201@gmail.com>
2025-11-14 11:25:25 +0100

    Lavet en hund

--- systemudvikling/Program.cs
+++ systemudvikling/Program.cs
@@ -2,3 +2,12 @@
 Console.WriteLine("Hello, World!");
 
 Console.WriteLine("Let me in!");
+
+public class Dog
+{
+    public Dog(string name)
+    {
+        Name = name;
+    }
+    string Name { get; set; }
+}
